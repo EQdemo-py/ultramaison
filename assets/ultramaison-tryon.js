7a5edb0ef36e59b08c6fb7c3f82a50718fab87ca
EQdemo-py <shopifybasic026@gmail.com>
2026-09-07 18:30:12 +0000

    Usar camara trasera por defecto en Try-On

--- assets/ultramaison-tryon.js
+++ assets/ultramaison-tryon.js
@@ -19,9 +19,9 @@ class UltramaisonTryOn {
     try {
       this.stream = await navigator.mediaDevices.getUserMedia({
         video: {
-          facingMode: 'user',
-          width: { ideal: 1280 },
-          height: { ideal: 720 }
+          facingMode: { ideal: 'environment' },
+          width: { ideal: 1920 },
+          height: { ideal: 1080 }
         },
         audio: false
       });
